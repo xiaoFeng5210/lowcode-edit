2823618bb908ada043f351c764563df9a986bf15
qingfeng.zhang <804875728@qq.com>
2021-06-02 00:38:42 +0800

    fix

---
+++ src/components/picture/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="overflow: hidden">
+        <img :src="propValue" alt="">
+    </div>
+</template>
+
+<script lang="ts">
+    import { ref, defineComponent, reactive } from 'vue'
+    export default defineComponent({
+        name: 'picture',
+        props: {
+          propValue: String
+        },
+        setup: () => {
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="g_main">
-        主页面
+    <div class="g_box">
+        <Picture :propValue="url"></Picture>
     </div>
 </template>
 
 <script lang="ts">
-    import {defineComponent, ref} from "vue";
+    import {defineComponent, ref, reactive} from "vue";
+    import Picture from '@/components/picture/index.vue'
     export default defineComponent({
         name: 'main',
         components: {
+            Picture
         },
         setup: () => {
-
+            const url = ref('12345');
+            return { url }
         }
     })
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.g_box {
+    background-color: #fff;
+}
 </style>
